5bb8f6f343d54e2295ecbea8f91ce1d9763bfcef
SnIvanova <1snizhanaivanova@gmail.com>
2024-03-24 23:50:50 +0100

    maitanance

--- GymApp/resources/views/activities/activities.blade.php
+++
@@ -1,6 +0,0 @@
-<h1>My Activities</h1>
-<ul>
-    @foreach($activities as $activity)
-        <li>{{ $activity->name }}</li>
-    @endforeach
-</ul>
--- GymApp/resources/views/reservations/reservations.blade.php
+++
@@ -1,6 +0,0 @@
-<h1>My Reservations</h1>
-<ul>
-    @foreach($reservations as $reservation)
-        <li>{{ $reservation->activity->name }} - {{ $reservation->reserved_at }}</li>
-    @endforeach
-</ul>
